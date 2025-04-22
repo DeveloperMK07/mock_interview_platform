1657ed9662faf0a599f486802a611bed0d3de947
unknown <mayur7024k@gmail.com>
2025-04-22 18:34:44 +0530

    implement interview page

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,17 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+        <h3>Interview Generation</h3>
+
+        <Agent userName= 'You'
+        userId='user1'
+        // profileImage={user?.profileURL}
+        type="generate"/>
+    </>
+  )
+}
+
+export default page
